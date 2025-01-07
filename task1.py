6f7409bace2afcedbdb2acab57d17513982ad26b
Ellenz123 <ellenz759@deltalearns.ca>
2025-01-07 11:25:51 -0800

    I am finished.

--- task1.py
+++ task1.py
@@ -1,6 +1,17 @@
 def A():
+    print("Hello")
 
 def B():
+    print("How are you")
 
 
 def C():
+    print("Hi")
+
+x=input("Enter a letter from A to C:")
+if x == "A":
+    A()
+if x == "B":
+    B()
+if x == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,22 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+
+def title():
+    print("This is a number guessing game.")
+    print("Enter a number between 1 and 20 in order to guess the secret number!")
+    print("You have only one chance to guess. Good luck!")
+
+title()
+
+def game():
+    number=random.choice(range(1,50))
+    guess=input("Enter an integer between 1 and 20:")
+    guess=int(guess)
+    if guess== number:
+        print("Congratulations! That is the right number!")
+    elif guess != number:
+        print("You lose!!")
+        
+game()
