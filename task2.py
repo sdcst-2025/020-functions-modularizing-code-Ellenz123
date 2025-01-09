cfc12e61a6089dcfb6badf82a82d31cbcb710bc5
Ellenz123 <ellenz759@deltalearns.ca>
2025-01-09 10:50:58 -0800

    I am finished.

--- task2.py
+++ task2.py
@@ -13,17 +13,23 @@ import random
 def title():
     print("This is a number guessing game.")
     print("Enter a number between 1 and 20 in order to guess the secret number!")
-    print("You have only one chance to guess. Good luck!")
 
 title()
 
 def game():
-    number=random.choice(range(1,50))
-    guess=input("Enter an integer between 1 and 20:")
-    guess=int(guess)
-    if guess== number:
-        print("Congratulations! That is the right number!")
-    elif guess != number:
-        print("You lose!!")
+    number=random.choice(range(1,20))
+    while True:
+        try:
+             guess=input("Enter an integer between 1 and 20:")
+             guess=int(guess)
+             if guess== number:
+               print("Congratulations! That is the right number!")
+               break
+             elif guess< number:
+                  print("Try a larger number.")
+             else:
+                 print("Try a smaller number.")
+        except:
+            print("Enter a valid integer.")
         
 game()
